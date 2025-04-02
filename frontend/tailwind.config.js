b167c1bbb6978fbeca32a9b0b17c36587b24d79e
Raman-Cz <ramankemwal@gmail.com>
2025-04-02 21:33:17 +0530

    updated daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,4 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -7,17 +6,5 @@ export default {
 		extend: {},
 	},
 	plugins: [daisyui],
-
-	daisyui: {
-		themes: [
-			"light",
-			{
-				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
-				},
-			},
-		],
-	},
 };
+
